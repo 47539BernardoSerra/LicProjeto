92e5e136a2505661bf1dcc21fd0cd77ed767b59a
Dragoral <66282735+Dragoral@users.noreply.github.com>
2021-05-28 09:03:06 +0100

    Door update

---
+++ src/main/kotlin/Door.kt
@@ -0,0 +1,21 @@
+object Door {
+    //controla o mecanismo da porta
+
+    // Inicia a classe, estabelecendoos valores iniciais.
+    fun init(){
+
+    }
+    // Envia comando paraabrir a porta, indicando a velocidade
+    fun open(speed: Int){
+
+    }
+    // Envia comando para fechar a porta, indicando a velocidade
+    fun close(speed: Int){
+
+    }
+
+    // Retorna true se o tiverterminado o comando
+    fun isFinished(){
+
+    }
+}
