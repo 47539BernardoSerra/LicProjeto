7df0fae980667f0d41a4572932373342faf50c9c
Dragoral <dragoral.wizard@gmail.com>
2021-06-06 20:46:11 +0100

    Bora Bora

--- src/main/kotlin/Log.kt
+++ src/main/kotlin/Log.kt
@@ -2,11 +2,16 @@ import java.io.File
 
 object Log {
 
-    private const val LOG_FILE = "LOG.txt"
-    private const val USER_FILE = "USERS.txt"
+    //o Log regista num ficheiro a posicao dos users (se entraram ou asiram da ultima vez) e horas.
 
+    private const val LOG_FILE = "LOG.txt"
+
+    //Listas usadas para manter registo da posição dos users em relação a entradas e saidas.
     private var listOfInside = mutableMapOf<String, Boolean>()
     private var listOfTime = mutableMapOf<String,String>()
+
+    //Intializa as listas usadas, mediante o ficheiro prexistente de LOG. Para correr optimamente, é preferivel que se guardaem os logs à parte e se use um ficheiro em branco cada vez que se recomeçao programa,
+    //no entanto, em caso de crash ou erro, este pode ser começado com os logs do dias graças a isto.
 
     fun init() {
 
@@ -29,11 +34,14 @@ object Log {
 
         } while (bfLog.ready())
     }
+    //Transforma a arrow guardada em logs no booleano para saber se o user setá dentro ou fora das portas.
 
     private fun inside(arrow: String): Boolean = arrow == "->"
 
+    //Transforma o booleano na flecha correspondente para logs
     private fun boolToArrow(boolean: Boolean?): String = if (boolean == true) "->" else "<-"
 
+    //Escreve nos logs
     fun writeLog(uin: Int, name: String) {
         listOfInside[uin.toString()] = !listOfInside[uin.toString()]!!
         val idIn = listOfInside[uin.toString()]
@@ -42,8 +50,10 @@ object Log {
         File(LOG_FILE).appendText("\n"+toWrite)
     }
 
+    //Diz se o user está a entrar ou a sair com a flecha
     fun entering(uin: String) : String = if(listOfInside[uin]!=null) boolToArrow(listOfInside[uin]!!) else "NONE"
 
+    //Diz a ultima vez que um determinado user passou as portas.
     fun lastRegisterTime(uin: String):String = if(listOfTime[uin]!=null) listOfTime[uin]!! else "NONE"
 
 }
